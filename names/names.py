b448dde6f369906e75d1998319260a798af6bba0
Leila <nb.leila10@gmail.com>
2019-09-14 11:35:08 -0500

    Use a dictionary to improve the runtime

--- names/names.py
+++ names/names.py
@@ -11,11 +11,18 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Complexity: O(n^2) - double for loops going through all the elements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
+# Runtime complexity: O(n) - only one for loop going through all the elements
+# Getting key from dict is O(1)
+name_dict = {name:1 for name in names_2}
+for name_1 in names_1:
+    if name_1 in name_dict:
+        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
